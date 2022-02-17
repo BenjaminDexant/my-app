9fbf86404f2ae9403994d1769533c376b314ecfb
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2022-02-17 14:15:07 +0100

    add camera

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,10 +1,30 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-        <Text>Camera Screen !</Text>
-    </View>
+        <Camera style={styles.camera} type={type} />
   );
 };
 
+const styles = StyleSheet.create({
+  camera: {flex: 1}
+}); 
