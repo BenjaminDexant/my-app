b4c4363e208040c7333ffd0cbd4c3653d5cfae80
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2022-02-17 12:22:41 +0100

    add antdesign icons for tabs icons

--- App.tsx
+++ App.tsx
@@ -1,38 +1,50 @@
+import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+
+import CameraScreen from './screens/CameraScreen';
+import FeedScreen from './screens/FeedScreen';
+import ImageScreen from './screens/ImageScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.globalcontainer}>
-      <View style={styles.containera}>
-        <Text style={styles.text}>Hello, world!</Text>
-        <Text style={styles.text}>Hello, world!</Text>
-        <Text style={styles.text}>Hello, world!</Text>
-        <Button title="This is a button" />
-      </View>
-      <View style={styles.containerb}>
-        <Text style={styles.text}>Hello, world!</Text>
-        <Text style={styles.text}>Hello, world!</Text>
-        <Text style={styles.text}>Hello, world!</Text>
-      </View>
-    </View>
-  );
-};
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string | undefined;
 
-const styles = StyleSheet.create({
-  text: {
-    color: "blue",
-  },
-  containera: {
-    backgroundColor: "grey",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  containerb: {
-    backgroundColor: "lightgreen",
-    alignItems: "flex-end",
-  },
-  globalcontainer: {
-    flex: 1,
-  },
-});
+            if (route.name === 'Camera') {
+              iconName = focused
+                ? 'camerao'
+                : 'camera';
+            } else if (route.name === 'Image') {
+              iconName = "picture";
+            } else if (route.name === 'Feed') {
+               iconName = focused
+                ? 'aliwangwang'
+                : 'aliwangwang-o1';
+            }
+  
+
+            // You can return any component that you like here!
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+      })}
+      >
+        <Tab.Screen
+          name="Camera"
+          component={CameraScreen}
+          options={{ unmountOnBlur: true }}
+        />
+        <Tab.Screen name="Image" component={ImageScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>  );
+};
