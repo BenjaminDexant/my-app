6a50b461da2f6d6c8846f9605ce123ed02c9ff81
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2022-02-17 15:35:44 +0100

    add flatlist core component to render images taken in cache

--- screens/ImageScreen.tsx
+++ screens/ImageScreen.tsx
@@ -1,10 +1,40 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, StyleSheet, FlatList } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 export default function ImageScreen() {
+    const [gallery, setGallery] = useState([""]);
+
+    useEffect(() => {
+        (async () => {
+          const images = await FileSystem.readDirectoryAsync(
+            FileSystem.cacheDirectory + "ImageManipulator"
+          );
+          setGallery(images);
+        })();
+      }, []);
+
   return (
-    <View>
-        <Text>Image Screen !</Text>
-    </View>
-  );
+    <FlatList
+      data={gallery}
+      keyExtractor={(gallery) => gallery}
+      renderItem={(image) => {
+        return (
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                FileSystem.cacheDirectory + "ImageManipulator/" + image.item,
+            }}
+          />
+        );
+      }}
+    />);
 };
 
+const styles = StyleSheet.create({
+    image: {
+      resizeMode: "cover",
+      height: 500,
+    },
+  });
