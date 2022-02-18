1d1017555e29179f225cbbc08791cc770900e252
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2022-02-18 10:49:17 +0100

    add upload fecth function to display all image from server

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,10 +1,42 @@
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FlatList, Image, StyleSheet } from "react-native";
 
 export default function FeedScreen() {
+
+  const [gallery, setGallery] = useState<string[]>([""]);
+
+  useEffect(() => {
+    (async () => {
+      const remoteFiles = await axios.get(
+        "https://wildstagram.nausicaa.wilders.dev/list"
+      );
+      console.log("Remote files", remoteFiles.data);
+      setGallery(remoteFiles.data);
+    })();
+  }, []);
+
   return (
-    <View>
-        <Text>Feed Screen !</Text>
-    </View>
-  );
+    <FlatList
+      data={gallery}
+      keyExtractor={(gallery) => gallery}
+      renderItem={(image) => {
+        return (
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+              "https://wildstagram.nausicaa.wilders.dev/files/" + image.item,
+            }}
+          />
+        );
+      }}
+    />);
 };
 
+const styles = StyleSheet.create({
+    image: {
+      resizeMode: "cover",
+      height: 500,
+    },
+  });
