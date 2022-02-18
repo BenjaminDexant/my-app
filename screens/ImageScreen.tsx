eda6f7151f506e57351b4433b31f2e64ed3d3681
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2022-02-18 10:48:35 +0100

    add upload image function to the wild server

--- screens/ImageScreen.tsx
+++ screens/ImageScreen.tsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, FlatList } from 'react-native';
+import { Button, Image, StyleSheet, FlatList } from 'react-native';
 import * as FileSystem from 'expo-file-system';
+import singleFileUploader from "single-file-uploader";
 
 export default function ImageScreen() {
-    const [gallery, setGallery] = useState([""]);
+    const [gallery, setGallery] = useState<string[]>([""]);
+
+    const handlePress = async (item: string) => {
+      try {
+        singleFileUploader(
+            {
+               distantUrl:"https://wildstagram.nausicaa.wilders.dev/upload",
+               filename: item,
+               filetype: "image/jpeg",
+               formDataName: "fileData",
+               localUri: FileSystem.cacheDirectory + "ImageManipulator/" + item,
+        });
+        alert("Image uploaded successfully");
+      } catch (error) {
+        alert("Error while uploading image");
+      }
+    };    
 
     useEffect(() => {
         (async () => {
@@ -12,7 +29,7 @@ export default function ImageScreen() {
           );
           setGallery(images);
         })();
-      }, []);
+    }, []);
 
   return (
     <FlatList
@@ -20,13 +37,22 @@ export default function ImageScreen() {
       keyExtractor={(gallery) => gallery}
       renderItem={(image) => {
         return (
-          <Image
-            style={styles.image}
-            source={{
-              uri:
-                FileSystem.cacheDirectory + "ImageManipulator/" + image.item,
-            }}
-          />
+            <>
+                <Image
+                    style={styles.image}
+                    source={{
+                        uri:
+                        FileSystem.cacheDirectory + "ImageManipulator/" + image.item,
+                    }}
+                />
+                <Button
+                    title='Upload image'
+                    onPress={() => handlePress(image.item)}
+                    color="grey"
+                    accessibilityLabel="Upload image"
+                />
+
+            </>
         );
       }}
     />);
